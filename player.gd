23661555a630377fa66b5b13d2ac44adae8b2f42
Just Jon <jonnathaniel1@gmail.com>
2026-05-28 10:32:04 -0400

    spaceshipMovement-2
    Spaceship has basic movement. W and D to move forwards and backwards. A and D to rotate left and right.
    
    Updated updates and planning documentation.
    
    Commented future plans after prototype

--- player.gd
+++ player.gd
@@ -1,9 +1,22 @@
 extends CharacterBody2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+@export var speed = 400
+@export var rotationSpeed = 1.5
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var rotationDirection = 0
+
+func get_input():
+	rotationDirection = Input.get_axis("moveLeft", "moveRight")
+	# !Modify this to have moving backwards as an "upgrade"
+	velocity = transform.x * Input.get_axis("moveDown", "moveUp") * speed
+	# While velocity is greater than zero
+		# decrement velocity as spacebar is pressed
+	
+func _physics_process(delta):
+	get_input()
+	rotation += rotationDirection * rotationSpeed * delta
+	move_and_slide()
+
+# !!!Improve movement
+	# Inertia when rotating
+	# Movement not stopping unless brakes are applied, and even then it just dampens speed
